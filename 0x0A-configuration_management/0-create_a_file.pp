3d19de387d0f59603d601be0506f0563ab5a3c2b
Noransaber <noransaber685@gmail.com>
2023-09-22 13:33:15 +0300

    Updates

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # Create a file in /tmp
 
-file {'/tmp/school':,
-mode => '0644',
-owner => 'www-data',
-group => 'www-data',
-content => 'I love Puppet'
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
-
 
