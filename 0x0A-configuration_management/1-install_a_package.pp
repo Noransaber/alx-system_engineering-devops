17495cea99e25e9c9470f15675eaf0059b699848
Noransaber <noransaber685@gmail.com>
2023-09-22 13:56:53 +0300

    Updates

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
+#!/usr/bin/pup
 # install flask
-
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
