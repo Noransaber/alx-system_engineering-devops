0c46abcea87fa042bf9658db58b1ceea7d5eac29
Noransaber <noransaber685@gmail.com>
2023-09-24 03:37:04 +0300

    Updates

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 #we’d like you to set up your client SSH configuration
-# file so that you can connect to a
-# server without typing a password.
-
-file_line {'Turn off passwd auth':
-    ensure => 'present',
-    path => '/etc/ssh/ssh_config',
-    line => '    PasswordAuthentication no',
-}
-
-file_line {{ 'Declare identity file':
+file_line { 'Identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school',
+}
+file_line { 'disable password login':
+    path    => '/etc/ssh/ssh_config',
+    line    => '    PasswordAuthentication no',
 }
