086a17d50148fbc4d98f0a15f2895e08fa85139a
Noransaber <noransaber685@gmail.com>
2023-11-08 14:46:00 +0200

    Updates

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => ['/bin', '/usr/bin', '/usr/sbin'],
-  onlyif  => "test -f /var/www/html/wp-settings.php",  # Add this line to check if the file exists
+  onlyif  => 'test -f /var/www/html/wp-settings.php'
 }
